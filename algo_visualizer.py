b7a013e44b452040c2200e63f84ae5e5e4945f1e
Akash <aviswa@macbookpro.mynetworksettings.com>
2025-03-24 20:13:27 -0400

    added reset and delay slider

--- algo_visualizer.py
+++ algo_visualizer.py
@@ -4,7 +4,6 @@ import random
 WIDTH = 800
 HEIGHT = 400
 BAR_WIDTH = 8
-DELAY = 10  # milliseconds (smaller = faster)
 
 class AlgoVisualizer:
     def __init__(self, master):
@@ -17,11 +16,23 @@ class AlgoVisualizer:
         self.data = [random.randint(10, HEIGHT) for _ in range(WIDTH // BAR_WIDTH)]
         self.draw_bars(self.data)
 
+        # Sorting state
         self.i = 0
         self.j = 0
 
-        start_button = tk.Button(master, text="Start Bubble Sort", command=self.bubble_sort_step)
-        start_button.pack(pady=10)
+        # Controls
+        controls_frame = tk.Frame(master)
+        controls_frame.pack(pady=10)
+
+        self.delay_slider = tk.Scale(controls_frame, from_=1, to=200, label="Delay (ms)", orient=tk.HORIZONTAL)
+        self.delay_slider.set(20)  # Default speed
+        self.delay_slider.pack(side=tk.LEFT, padx=10)
+
+        start_button = tk.Button(controls_frame, text="Start Bubble Sort", command=self.bubble_sort_step)
+        start_button.pack(side=tk.LEFT, padx=10)
+
+        reset_button = tk.Button(controls_frame, text="Reset", command=self.reset)
+        reset_button.pack(side=tk.LEFT, padx=10)
 
     def draw_bars(self, data, color_array=None):
         self.canvas.delete("all")
@@ -46,16 +57,22 @@ class AlgoVisualizer:
 
                 if self.data[self.j] > self.data[self.j + 1]:
                     self.data[self.j], self.data[self.j + 1] = self.data[self.j + 1], self.data[self.j]
-                
+
                 self.j += 1
                 self.draw_bars(self.data, color_array)
-                self.master.after(DELAY, self.bubble_sort_step)
+                self.master.after(self.delay_slider.get(), self.bubble_sort_step)
             else:
                 self.j = 0
                 self.i += 1
-                self.master.after(DELAY, self.bubble_sort_step)
+                self.master.after(self.delay_slider.get(), self.bubble_sort_step)
         else:
             self.draw_bars(self.data, ["green"] * len(self.data))
+
+    def reset(self):
+        self.data = [random.randint(10, HEIGHT) for _ in range(WIDTH // BAR_WIDTH)]
+        self.i = 0
+        self.j = 0
+        self.draw_bars(self.data)
 
 # Run GUI
 if __name__ == "__main__":
